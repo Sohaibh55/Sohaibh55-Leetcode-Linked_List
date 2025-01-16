255fe682e3272d082663e612820dc731ed640499
sohaibh55 <sohaibhamdi55@outlook.com>
2025-01-16 17:54:50 -0400

    new project

---
+++ Solution.cpp
@@ -0,0 +1,38 @@
+//
+// Created by sohai on 1/14/2025.
+//
+
+#include "Solution.h"
+ListNode* Solution::reverseList(ListNode* head)
+{
+    ListNode* currrentNode = head;
+    ListNode* previous = nullptr;
+    ListNode* restofList = head->next;
+    while (restofList != nullptr) {
+        currrentNode->next = previous;
+        previous = currrentNode;
+        currrentNode = restofList;
+        restofList = restofList->next;
+    }
+    currrentNode->next = previous;
+    return currrentNode;
+
+}
+ListNode* Solution::reverseList2(ListNode* head) {
+    if (!head) {
+        return nullptr;
+    }
+
+    ListNode* newHead = head;
+    if (head->next) {
+        newHead = reverseList2(head->next);
+        head->next->next = head;
+    }
+    head->next = nullptr;
+
+    return newHead;
+}
+ListNode* Solution::mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+
+}
