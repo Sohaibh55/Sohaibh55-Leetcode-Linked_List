0b45f6f278abc5a65842318889c353a1ab50355c
sohaibh55 <sohaibhamdi55@outlook.com>
2025-01-16 17:58:09 -0400

    header

---
+++ Solution.h
@@ -0,0 +1,44 @@
+//
+// Created by sohai on 1/14/2025.
+//
+
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void insert(ListNode* &head,int value)
+ {
+    ListNode* nouveau = new ListNode(value);
+   if(head == nullptr)
+      head = nouveau ;
+   else
+    {
+      nouveau->next = head;
+      head = nouveau;
+    }
+ }
+
+
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head);
+    ListNode* reverseList2(ListNode* head);
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+};
+
+
+#endif //SOLUTION_H
